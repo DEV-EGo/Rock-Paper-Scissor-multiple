dc5619f07965fadb6fe96482ba85140c5556075b
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-12-17 13:03:54 -0500

    practice day 6

--- Javascript/JS_2.js
+++ Javascript/JS_2.js
@@ -33,3 +33,39 @@ document.onkeyup = function ( event ) {
         ComputerChoiceText.textContent = "the computer chose:" + ComputerGuess;
     }
 };
+
+
+// more practice
+// creating new array
+var LettersToTap =[
+    "w",
+    "a",
+    "s"
+];
+
+// variables that will target the html
+
+var GamerChoice = document.getElementById("Game-text");
+var AIGuess = document.getElementById("AI-text");
+
+// function will run when a key is triggered
+
+document.onkeyup = function(event) {
+
+    // determines what key was hit
+    var GamerGuess = event.key;
+
+    // this code will only choose randomly from the array
+    var aiGuessing = LettersToTap[Math.floor(Math.random() * LettersToTap.length)];
+
+    // this code will only run if these letters are tapped
+    if(
+        (GamerGuess === "w") || (GamerGuess === "a") || (GamerGuess === "s")
+    )
+    {
+        // this line of code will display the results
+        GamerChoice.textContent = "test chose:" + GamerGuess;
+        AIGuess.textContent = "robot chose:" +aiGuessing;
+
+    }
+}
