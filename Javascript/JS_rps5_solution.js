20b9af58a8d91b16a915122d40693d673e0871df
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-12-21 13:04:43 -0500

    new variables

--- Javascript/JS_rps5_solution.js
+++ Javascript/JS_rps5_solution.js
@@ -7,3 +7,17 @@ var computerChoices = [
 
 // creating variables to hold the number of wins-losses-ties.
 // they start at 0
+
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+// variables that hold the reference to the places in the html where we want to display
+// things...
+
+var directionsText = document.getElementById("directions_text");
+var userchoiceText = document.getElementById("userchoice_text");
+var copmuterchoiceText = document.getElementById("computerchoice_text");
+var winsText= document.getElementById("wins_text");
+var lossesText = document.getElementById("losses_text");
+var tiesText = document.getElementById("ties_text");
