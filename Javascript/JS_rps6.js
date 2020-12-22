aa961dc3e3ace7a13b0d0bea83e17a459192f4ac
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-12-21 22:23:16 -0500

    new variables

--- Javascript/JS_rps6.js
+++ Javascript/JS_rps6.js
@@ -1,1 +1,20 @@
-alert("test");
+// creating array of choices
+var ChoiceToChooseFrom = [
+    "r",
+    "p",
+    "s"
+];
+
+// creating variables to hold record of wins-losses-ties
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+// variables that will target the htmnl that will display info
+
+var TextForDirections = document.getElementById("Direction-text");
+var TextForChoice = document.getElementById("UserChoice");
+var TextForAiChoice = document.getElementById("ChoiceByComputer");
+var TextForWins = document.getElementById("RecordOfWins");
+var TextForLosses = document.getElementById("RecordOFLosses");
+var TextForTies = document.getElementById("RecordOfTies");
