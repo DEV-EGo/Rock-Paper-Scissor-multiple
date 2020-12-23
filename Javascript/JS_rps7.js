088e73d7e95d937b129f8e9188071e4d41170a19
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-12-23 12:33:33 -0500

    if else statement

--- Javascript/JS_rps7.js
+++ Javascript/JS_rps7.js
@@ -1,1 +1,53 @@
-// 
+// array storing variables
+var PickFromThis = [
+    "r",
+    "p",
+    "s"
+];
+
+// variables that will hold the scores
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+// variables targeting the point of reference in the html
+var TargetDirections = document.getElementById("Directions");
+var TargetPlayersChoice = document.getElementById("PlayersChoice");
+var TargetAIChoice = document.getElementById("CompChoice");
+var TargetWins = document.getElementById("Wins");
+var TargetLosses = document.getElementById("Losses");
+var TargetTies = document.getElementById("Ties");
+
+// Function runs when the user presses a key
+
+document.onkeyup - function (event) {
+    // determines what key is being pressed
+    var UserGuess = event.key;
+
+    // Randomizing a choice from the array option
+    var CompGuess = PickFromThis[Math.floor(Math.random() * PickFromThis)];
+
+    // if else statement 
+    if((UserGuess === "r") || (UserGuess === "p") || (UserGuess === "s")){
+        if ((UserGuess === "r" && CompGuess === "s") || 
+        (UserGuess === "s" && CompGuess === "p") ||
+        (UserGuess === "p" && CompGuess === "r")) {
+            wins++;
+        } else if (UserGuess === CompGuess) {
+            ties++;
+        }else{
+            losses++;
+        }
+
+        // hide the directions
+        TargetDirections.textContent ="";
+
+        // displaying the user and computer stats
+
+        TargetPlayersChoice.textContent = "you chose" + UserGuess;
+        TargetAIChoice.textContent = " computer chose" + CompGuess;
+        TargetWins.textContent = " wins:" +wins;
+        TargetLosses.textContent = " losses:" + losses;
+        TargetTies.textContent = " Ties:" + ties;
+    }
+}
